e4025fa0a0a1e9a5cdcc5ee2a8fe675bb9337dae
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2021-12-20 18:46:01 -0500

    initiated database by creating db, seeds and schema sql files created connection.js to link database to the inquirer prompts

---
+++ db/connection.js
@@ -0,0 +1,15 @@
+// import mysql2
+const mysql = require('mysql2');
+
+// connect to database 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Ilovedogs123',
+        database: 'company'
+    },
+    console.log('connected to the election database.')
+);
+
+module.exports = db;
